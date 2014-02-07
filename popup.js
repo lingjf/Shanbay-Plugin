6d8fba0f7fe3151fbbef56a29ade1951fba04449
lingjf <lingjf@gmail.com>
2014-02-07 20:33:18 +0800

    Add unit test, and wildcard query

--- popup.js
+++ popup.js
@@ -1,5 +1,23 @@
-var selectWord = null;
-var selectObject = null;
+
+var M = {};
+
+function resetM() {
+	M = {
+		word : null,
+		candidate : null,
+		vocabulary : null,
+		pronunciation : null,
+		audio : null,
+		definition : null,
+		learning_id : null,
+		retention : null,
+		target_retention : null,
+		percentage : null,
+		error_msg : null,
+		login_shanbay : null
+	};
+}
+
 
 function loginShanbay() {
 	chrome.tabs.create({
@@ -9,8 +27,8 @@ function loginShanbay() {
 
 
 function reviewWord_shanbay() {
-	if (selectObject && selectObject.learning_id) { 
-		var url = "http://www.shanbay.com/review/learning/" + selectObject.learning_id;
+	if (M.learning_id) { 
+		var url = "http://www.shanbay.com/review/learning/" + M.learning_id;
 		chrome.tabs.create({
 			url : url,
 			selected : true
@@ -19,8 +37,8 @@ function reviewWord_shanbay() {
 }
 
 function reviewWord_iciba() {
-	if (selectObject && selectObject.content) { 
-		var url = "http://www.iciba.com/" + selectObject.content;
+	if (M.vocabulary) { 
+		var url = "http://www.iciba.com/" + M.vocabulary;
 		chrome.tabs.create({
 			url : url,
 			selected : true
@@ -29,8 +47,8 @@ function reviewWord_iciba() {
 }
 
 function reviewWord_youdao() {
-	if (selectObject && selectObject.content) { 
-		var url = "http://dict.youdao.com/search?q=" + selectObject.content;
+	if (M.vocabulary) { 
+		var url = "http://dict.youdao.com/search?q=" + M.vocabulary;
 		chrome.tabs.create({
 			url : url,
 			selected : true
@@ -39,8 +57,8 @@ function reviewWord_youdao() {
 }
 
 function reviewWord_vocabulary() {
-	if (selectObject && selectObject.content) { 
-		var url = "http://www.vocabulary.com/dictionary/" + selectObject.content;
+	if (M.vocabulary) { 
+		var url = "http://www.vocabulary.com/dictionary/" + M.vocabulary;
 		chrome.tabs.create({
 			url : url,
 			selected : true
@@ -49,8 +67,8 @@ function reviewWord_vocabulary() {
 }
 
 function reviewWord_etymology() {
-	if (selectObject && selectObject.content) { 
-		var url = "http://www.etymonline.com/index.php?term=" + selectObject.content;
+	if (M.vocabulary) { 
+		var url = "http://www.etymonline.com/index.php?term=" + M.vocabulary;
 		chrome.tabs.create({
 			url : url,
 			selected : true
@@ -59,8 +77,8 @@ function reviewWord_etymology() {
 }
 
 function forgetWord() {
-	if (selectObject && selectObject.learning_id) { 
-		var url = "http://www.shanbay.com/api/v1/bdc/learning/" + selectObject.learning_id;
+	if (M.vocabulary) { 
+		var url = "http://www.shanbay.com/api/v1/bdc/learning/" + M.vocabulary;
 		$.ajax({
 			url : url,
 			type : 'PUT',
@@ -70,8 +88,10 @@ function forgetWord() {
 			success : function(data) {
 				console.log(data);
 				if (data.status_code == 0 && data.msg == "SUCCESS") {
-					// window.location.reload();
-					queryWord(selectObject.content);
+					queryWord(M.vocabulary);
+				} else {
+					M.error_msg = "操作失败. " + data.msg;
+					render();
 				}
 			},
 			error : function() {
@@ -85,8 +105,8 @@ function forgetWord() {
 }
 
 function addingWord() {
-	if (selectObject && selectObject.content) { 
-		var url = "http://www.shanbay.com/api/learning/add/" + selectObject.content;
+	if (M.vocabulary) { 
+		var url = "http://www.shanbay.com/api/learning/add/" + M.vocabulary;
 		$.ajax({
 			url : url,
 			type : 'GET',
@@ -96,12 +116,16 @@ function addingWord() {
 				console.log(data);
 				if (data['id']) {
 					// window.location.reload();
-					queryWord(selectObject.content);
+					queryWord(M.vocabulary);
+				} else {
+					M.error_msg = "添加失败.";
+					render();
 				}
 			},
 			error : function() {
-				$("#error_msg").html("添加失败，<br>可能没有<a href='#'>登录扇贝网</a>。").show();
-				$("#error_msg").click(loginShanbay);
+				M.error_msg = "添加失败，<br>可能没有";
+				M.login_shanbay = true;
+				render();
 			},
 			complete : function() {
 				
@@ -110,15 +134,9 @@ function addingWord() {
 	}
 }
 
-function mp3Speak(utterance) {
-	var sound = new Howl({
-		urls: [selectObject.audio]
-	}).play();
-}
-
-
 function getWord() {
-	var url = "http://www.shanbay.com/api/v1/bdc/search/?word=" + selectWord;
+	var getting = M.word;
+	var url = "http://www.shanbay.com/api/v1/bdc/search/?word=" + M.word;
 	$.ajax({
 		url : url,
 		type : 'GET',
@@ -126,70 +144,51 @@ function getWord() {
 		contentType : "application/json; charset=utf-8",
 		success : function(data) {
 			console.log(data);
-			if (data.status_code == 0 && data.msg == "SUCCESS") {
-				selectObject = data.data;
-				showWord();
-			} else {
-				$('#definition').html(data.msg);
+			if (getting == M.word) {
+				if (data.status_code == 0 && data.msg == "SUCCESS") {
+
+					M.vocabulary = data.data.content;
+					M.pronunciation = data.data.pronunciation;
+					M.audio = data.data.audio;
+					M.definition = data.data.definition;
+					M.learning_id = data.data.learning_id;
+					M.retention = data.data.retention;
+					M.target_retention = data.data.target_retention;
+					var percentage = data.data.retention * 100.0 / data.data.target_retention;
+					if (percentage < 3.0) {
+						percentage = 3.0;
+					} else if (percentage > 100.0) {
+						percentage = 100.0;
+					}
+					M.percentage = percentage;
+				} else {
+					M.error_msg = data.msg;
+				}
+				render();
 			}
 		},
 		error : function() {
-			console.log("getWord error");
-			$("#error_msg").html("查询失败，<br>可能 . . . ").show();
+			if (getting == M.word) {
+				M.error_msg = "查询失败，<br>可能 . . . ";
+				render();
+			}
 		},
 		complete : function() {
-			console.log("getWord complete");
+			// console.log("getWord complete");
 		}
 	});
 }
 
-function showWord() {
-	$('#word').html(selectObject.content);
-	$('#pronunciation').html("[" + selectObject.pronunciation + "]");
-	$('#pronunciation').mouseenter(mp3Speak);
-
-	$('#definition').html(selectObject.definition.split('\n').join('<br>'));
-
-	if (selectObject.learning_id != undefined && selectObject.learning_id != 0) { 
-		if (selectObject.retention != undefined && selectObject.target_retention != undefined) { 
-			$('#current_retention').html(selectObject.retention);
-			$('#target_retention').html(selectObject.target_retention);
-
-			var percentage = selectObject.retention * 100.0 / selectObject.target_retention;
-			if (percentage < 3.0) {
-				percentage = 3.0;
-			} else if (percentage > 100.0) {
-				percentage = 100.0;
-			}
-			$('#current_retention').css("width", "" + percentage + "%");
-		}
-
-		$('#oldword').show();
-		
-	} else {
-		
-		$('#newword').show();
-	}	
-}
 
 function queryWord(w) {
-	selectWord = w;
-	if (selectWord !== undefined && selectWord !== null && selectWord.length > 0) {
-		$('#pp_heading, #pp_body').show();
-
-		$('#word').html(selectWord);
-		$('#pronunciation').html("");
-		$('#definition').html("<img src='image/inquire.gif'/>");
-
-		$('#oldword').hide();
-		$('#newword').hide();
-		$("#error_msg").hide();
-		
+	resetM();
+	M.word = w;
+	if (isValid(M.word)) {
 		getWord();
-		
 	} else {
-		$('#pp_heading, #pp_body').hide();
+		M.word = null;
 	}
+	render();
 }
 
 function onQuery() {
@@ -197,14 +196,85 @@ function onQuery() {
 
 	if (queried !== undefined && queried !== null) {
 		queried = queried.trim();
-		if (/^[a-zA-Z\s']+$/.test(queried)) {
-			queryWord(queried);
+		var candidate = getCanditate(queried, 120);
+		if (candidate.length == 1) {
+			queryWord(candidate[0]);
+		} else {
+			resetM();
+			M.candidate = candidate;
+			render();
 		}
 	}
 }
 
+function onSelect() {
+	queryWord($(this).text());
+}
+
+function pronunceWord() {
+	var sound = new Howl({urls: [M.audio] }).play();
+}
+
+function render() {
+	$('#pp_candidate, #pp_heading, #pp_body').hide();
+	$('#oldword, #newword').hide();
+	$("#error_msg").hide();
+
+	if (M.candidate != null && M.candidate.length > 1) {
+		$('#pp_candidate').empty();
+		for (i in M.candidate) {
+			var c = $("<a href='#' class='list-group-item'>" + M.candidate[i] +"</a>");
+			c.click(onSelect);
+			$('#pp_candidate').append(c);
+		}
+		$('#pp_candidate').show();
+	} else {
+		$('#pp_candidate').empty();
+		$('#pp_candidate').hide();
+	}
+
+	if (M.vocabulary != null && M.vocabulary.length > 1) {
+		$('#pp_heading, #pp_body').show();
+		$('#word').html(M.vocabulary);
+		$('#pronunciation').html("[" + M.pronunciation + "]").mouseenter(pronunceWord);
+		$('#definition').html(M.definition.split('\n').join('<br>'));
+		if (M.learning_id != undefined && M.learning_id != null && M.learning_id != 0) { 
+			if (M.retention != null && M.target_retention != null) { 
+				$('#current_retention').html(M.retention);
+				$('#target_retention').html(M.target_retention);
+				$('#current_retention').css("width", "" + M.percentage + "%");
+			}
+			$('#oldword').show();
+		} else {
+			
+			$('#newword').show();
+		}	
+
+	} else if (M.word != null && M.word.length > 1) {
+		$('#pp_heading, #pp_body').show();
+		
+		$('#word').html(M.word);
+		$('#pronunciation').html("");
+		$('#definition').html("<img src='image/inquire.gif'/>");
+	}
+
+	if (M.error_msg != null) {
+		$('#error_msg').html(M.error_msg).show();
+		if (M.login_shanbay) {
+			var c = $("<a href='#'>登录扇贝网</a>");
+			c.click(loginShanbay);
+			$("#error_msg").append(c);
+		}
+	} else {
+		$('#error_msg').html("");
+		$('#error_msg').hide();
+	}
+}
+
+
 $(document).ready(function() {
 	// document.execCommand('paste');
+	resetM();
 
 	if (preference.get().RealtimeQuery) {
 		$('#queryword').keyup(onQuery);
---
+++ test/literal_test.js
@@ -0,0 +1,33 @@
+test( "isWildCard", function() {
+  equal( isWildCard('abc'), false,"Passed!" );
+  equal( isWildCard('abc*'), true,"Passed!" );
+  equal( isWildCard('ab*c'), true,"Passed!" );
+  equal( isWildCard('a*bc'), true,"Passed!" );
+  equal( isWildCard('*abc'), true,"Passed!" );
+
+  equal( isWildCard('?abc'), true,"Passed!" );
+  equal( isWildCard('a?bc'), true,"Passed!" );
+  equal( isWildCard('ab?c'), true,"Passed!" );
+  equal( isWildCard('abc?'), true,"Passed!" );
+
+  equal( isWildCard('ab[cd]'), true,"Passed!" );
+});
+
+test( "wildCard2Regex", function() {
+  equal( wildCard2Regex('ab*c'), "ab.*c","Passed!" );
+  equal( wildCard2Regex('ab?c'), "ab.c","Passed!" );
+});
+
+test( "getCanditate", function() {
+
+	var e = [
+		"abaca",
+		"aback",
+		"abacus",
+		"abacuses",
+		"huckaback"
+	];
+
+	var a = getCanditate('abac*', 0);
+	deepEqual(a, e, "Passed!" );
+});
